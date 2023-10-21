94fe0b36b3ecb99be226395cfcc30c2c8847af3d
Nugraha Yudhasyah <nyudhasyah@gmail.com>
2023-10-21 14:02:38 +0700

    Icon add

---
+++ app/src/main/java/com/example/youtubeclone/SplashScreenActivity.java
@@ -0,0 +1,19 @@
+package com.example.youtubeclone;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        new Handler().postDelayed(() -> {
+            startActivity(new Intent(SplashScreenActivity.this, MainActivity.class));
+            finish();
+        }, 0);
+    }
+}
